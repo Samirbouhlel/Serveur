016a4574a37311f72531da4df01f9ea2c34c740a
Samirbouhlel <samirbouhlel1994@gmail.com>
2018-08-27 12:45:25 +0100

    first commit

---
+++ src/main/java/org/serveur/demo/ServeurApplication.java
@@ -0,0 +1,12 @@
+package org.serveur.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurApplication.class, args);
+	}
+}
---
+++ src/test/java/org/serveur/demo/ServeurApplicationTests.java
@@ -0,0 +1,16 @@
+package org.serveur.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServeurApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
